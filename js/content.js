d0ca0d115e9ca1945a44ad43bd5ebcff92808a15
goksiuta <maxmice@gmail.com>
2018-08-04 02:17:23 -0700

    VUE instances

---
+++ js/content.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    Header: 'Agreements on the Ethereum blockchain Github',
+    Subheader: 'Create agreements with ease and store safely on the blockchain.',
+    ButtonLink: 'Check our Prototype',
+  }
+})
