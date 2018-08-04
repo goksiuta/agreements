a678bcb516e67973d4329cc39ba9ff9c1527e985
goksiuta <maxmice@gmail.com>
2018-08-04 10:11:54 -0700

    Vue update

--- js/content.js
+++ js/content.js
@@ -4,5 +4,10 @@ var app = new Vue({
     Header: 'Agreements on the Ethereum blockchain Github',
     Subheader: 'Create agreements with ease and store safely on the blockchain.',
     ButtonLink: 'Check our Prototype',
+    menu: [
+      { value: 'Home', link: 'home' },
+      { value: 'How it works', link: 'how-it-works' },
+      { value: 'Prototype', link: 'prototype' },
+    ]
   }
 })
